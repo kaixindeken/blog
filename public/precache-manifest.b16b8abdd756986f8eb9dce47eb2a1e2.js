5c2040c78e415219c0f63b0cd30741111d50008b
zhangruian <zra981217@163.com>
2020-10-06 12:40:21 +0800

    加上前端吧

---
+++ public/precache-manifest.b16b8abdd756986f8eb9dce47eb2a1e2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a43bb288c03c99c8d4d1815c5b78c943",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06dc8dcfe5938baf2d00",
+    "url": "/static/css/2.8c2d9ae8.chunk.css"
+  },
+  {
+    "revision": "e1ec9c98655396527bdb",
+    "url": "/static/css/3.cd846464.chunk.css"
+  },
+  {
+    "revision": "6be2f26ea77ff4483b98",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "06dc8dcfe5938baf2d00",
+    "url": "/static/js/2.5dc783e9.chunk.js"
+  },
+  {
+    "revision": "e4a0d87becbcc6d09668875fe197b50f",
+    "url": "/static/js/2.5dc783e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1ec9c98655396527bdb",
+    "url": "/static/js/3.9a84356d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/3.9a84356d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d909b0c2a42e3cb48f5",
+    "url": "/static/js/4.0b657fab.chunk.js"
+  },
+  {
+    "revision": "6be2f26ea77ff4483b98",
+    "url": "/static/js/main.4e35c0f2.chunk.js"
+  },
+  {
+    "revision": "513a9b8c3d12fda1fc76",
+    "url": "/static/js/runtime-main.72c691d7.js"
+  }
+]);
